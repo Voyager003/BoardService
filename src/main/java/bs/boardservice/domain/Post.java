193c167d3c546815f5485c53de0386402fdb125e
voyager003 <miraclex000@gmail.com>
2023-04-06 15:09:04 +0900

    Feat: Post(게시물) entity 추가

---
+++ src/main/java/bs/boardservice/domain/Post.java
@@ -0,0 +1,39 @@
+package bs.boardservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+@Getter
+public class Post {
+    @Id @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime createdDate; // 게시물 작성 시간
+    private LocalDateTime modifiedDate; // 게시물 수정 시간
+
+    public Post() {
+    }
+
+    @Builder
+
+    public Post(String title, String content, User user,  LocalDateTime createdDate) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.createdDate = createdDate;
+    }
+}
