295617143747161565389698fc4f14da8a1b1d30
voyager003 <miraclex000@gmail.com>
2023-04-07 16:19:16 +0900

    Feat: 회원가입 검증로직 controller 추가

--- src/main/java/bs/boardservice/controller/UserController.java
+++ src/main/java/bs/boardservice/controller/UserController.java
@@ -1,17 +1,44 @@
 package bs.boardservice.controller;
 
-import bs.boardservice.application.UserDto;
+import bs.boardservice.application.UserForm;
+import bs.boardservice.application.service.UserService;
+import bs.boardservice.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
-    @GetMapping("/auth/join")
-    public String join(Model model) {
-        model.addAttribute("UserDto", new UserDto());
-        return "signup";
+    private final UserService userService;
+
+    @GetMapping(value = "/auth/join")
+    public String createForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "users/signup";
+    }
+
+    @PostMapping(value = "/auth/join")
+    public String create(@Validated @ModelAttribute UserForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "users/signup";
+        }
+
+        User user = User.builder()
+                .loginId(form.getLoginId())
+                .password(form.getPassword())
+                .name(form.getName()).build();
+
+        userService.join(user);
+        return "redirect:/";
     }
 
     @GetMapping("/auth/login")
