3aa62a53a0a08bf98c1c850fa1498226aadc3a0f
voyager003 <miraclex000@gmail.com>
2023-04-10 23:00:48 +0900

    Feat: 로그인, 로그아웃 기능 추가

--- src/main/java/bs/boardservice/controller/UserController.java
+++ src/main/java/bs/boardservice/controller/UserController.java
@@ -1,8 +1,12 @@
 package bs.boardservice.controller;
 
+import bs.boardservice.application.LoginForm;
+import bs.boardservice.application.SessionConst;
 import bs.boardservice.application.UserForm;
 import bs.boardservice.application.service.UserService;
 import bs.boardservice.domain.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
@@ -42,7 +47,38 @@ public class UserController {
     }
 
     @GetMapping("/auth/login")
-    public String login() {
-        return "signin";
+    public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm) {
+        return "users/signin";
+    }
+
+    @PostMapping("/auth/login")
+    public String login(@Validated @ModelAttribute("loginForm") LoginForm form,
+                        BindingResult result, HttpServletRequest request) {
+
+        if (result.hasErrors()) {
+            return "users/signin";
+        }
+
+        User loginUser = userService.login(form.getLoginId(), form.getPassword());
+
+        if (loginUser == null) {
+            result.reject("loginFail", "아이디 혹은 비밀번호가 맞지 않습니다.");
+            return "users/signin";
+        }
+
+        // 세션 유지 시, redirect
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, loginUser);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/auth/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
     }
 }
