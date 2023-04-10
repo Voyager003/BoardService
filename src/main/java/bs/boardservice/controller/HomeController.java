665b4fc730cfd6dba895c13e78637c9007a08a02
voyager003 <miraclex000@gmail.com>
2023-04-10 23:03:59 +0900

    Feat: 로그인 상태를 전달하는 컨트롤러 추가

---
+++ src/main/java/bs/boardservice/controller/HomeController.java
@@ -0,0 +1,25 @@
+package bs.boardservice.controller;
+
+import bs.boardservice.application.SessionConst;
+import bs.boardservice.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String loginHome(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser,
+                            Model model) {
+
+        if (loginUser == null) {
+            return "index";
+        }
+
+        model.addAttribute("user", loginUser);
+        return "users/signin_completed";
+    }
+}
+
