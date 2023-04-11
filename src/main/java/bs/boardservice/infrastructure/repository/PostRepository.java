3388241c849ded65311a9e5b3a750d368ac682c6
voyager003 <miraclex000@gmail.com>
2023-04-11 16:10:07 +0900

    Feat: PostRepository 추가(Spring data JPA)

---
+++ src/main/java/bs/boardservice/infrastructure/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package bs.boardservice.infrastructure.repository;
+
+import bs.boardservice.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
