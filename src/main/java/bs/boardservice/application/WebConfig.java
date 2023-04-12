2d5287630c59f0bac3072da728ca46ba2549a6ea
voyager003 <miraclex000@gmail.com>
2023-04-12 16:40:42 +0900

    Feat: WebConfig 추가 회원가입, 로그인, 게시판 이동, 에러 페이지를 인터셉터 예외로 추가한다.

---
+++ src/main/java/bs/boardservice/application/WebConfig.java
@@ -0,0 +1,18 @@
+package bs.boardservice.application;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/auth/login", "/auth/join", "/auth/logout", "/post",
+                        "/*.ico", "/css/**", "/error");
+    }
+}
